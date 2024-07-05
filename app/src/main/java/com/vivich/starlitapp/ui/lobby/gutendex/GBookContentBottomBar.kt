8c02bee37ab5a5b53e0bf1659007a17d90cb200d
Jammi-UwU-Jammy <jamclark102@gmail.com>
2024-07-05 11:12:34 -0400

    added book content bottom nav icons

--- app/src/main/java/com/vivich/starlitapp/ui/lobby/gutendex/GBookContentBottomBar.kt
+++ app/src/main/java/com/vivich/starlitapp/ui/lobby/gutendex/GBookContentBottomBar.kt
@@ -1,2 +1,47 @@
 package com.vivich.starlitapp.ui.lobby.gutendex
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.vivich.starlitapp.R
+import com.vivich.starlitapp.ui.theme.contentGrayMedium
+
+@Composable
+fun BookContentBottomBar(modifier: Modifier = Modifier) {
+    BottomAppBar {
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = { Icon(painter = painterResource(id = R.drawable.sun_line), contentDescription = "") }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = { Text(text = "A", style = contentGrayMedium, fontSize = 25.sp) }
+        )
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = { Icon(painter = painterResource(id = R.drawable.construction_24dp_fill0_wght400_grad0_opsz24), contentDescription = "") }
+        )
+    }
+}
+
+@Composable
+fun CustomiseTab(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun BottomBarPreview(modifier: Modifier = Modifier) {
+    BookContentBottomBar()
+}
--- app/src/main/java/com/vivich/starlitapp/ui/lobby/gutendex/GBookContentTopNav.kt
+++ app/src/main/java/com/vivich/starlitapp/ui/lobby/gutendex/GBookContentTopNav.kt
@@ -1,2 +1,44 @@
 package com.vivich.starlitapp.ui.lobby.gutendex
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.vivich.starlitapp.ui.theme.contentGraySmall
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookContentTop(
+    modifier: Modifier = Modifier
+){
+    TopAppBar(
+        navigationIcon = {
+            Icon(imageVector = Icons.Default.KeyboardArrowLeft, contentDescription ="")
+        },
+        title = {
+            Text(
+                modifier = modifier
+                    .fillMaxWidth(1f)
+                    .padding(horizontal = 10.dp),
+                text = "AAA",
+                style = contentGraySmall,
+                textAlign = TextAlign.Center
+            )
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun TopNavPreview(modifier: Modifier = Modifier) {
+    BookContentTop()
+}
